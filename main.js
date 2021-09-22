df40a81e91afdd989827ec564fcd7457c6a7c96f
Emilija92 <pkovacevicemilija@gmail.com>
2021-09-22 20:48:29 +0200

    nav

---
+++ main.js
@@ -0,0 +1,45 @@
+/********nav***********/
+
+let navBars = document.querySelector('.icon');
+let ul = document.querySelector('ul');
+
+navBars.addEventListener('click', responsiveNav);
+
+function responsiveNav() {
+    if(ul.className === 'topnav') {
+        ul.className += ' responsive';
+    } else {
+        ul.className = 'topnav';
+    }
+}
+
+/*************slider*************/
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
